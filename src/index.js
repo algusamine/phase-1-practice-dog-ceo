6921d04d2002d6e8e5393287ad7779c739a02d2f
Amine <algusamine@gmail.com>
2021-07-07 11:59:22 -0400

    commit tree a20a3242a399008fc009e88f45747c9cf3f79b28

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log('%c HI', 'color: firebrick')
+function fetchThemAll(){
+    fetch('https://dog.ceo/api/breeds/image/random/4')
+    .then(resp => resp.json())
+    .then(data => renderImgs(data))
+}
+
+function renderImgs(dogs){
+    console.log(dogs);
+    dogs.message.forEach(data=>{
+
+    let div =document.querySelector('#dog-image-container');
+    //let li = document.createElement('li');
+    let img = document.createElement('img');
+    div.append(img)
+    img.src = data
+    });
+}
+
+document.addEventListener('DOMContentLoaded', () =>{
+    fetchThemAll();
+})
